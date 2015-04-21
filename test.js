a9317c395222da312a554852286ece89c18f70a1
ForgiveMyAwfulEngilsh <471073043@qq.com>
2015-04-21 17:05:31 +0800

    getter and setter method

---
+++ test.js
@@ -0,0 +1,27 @@
+A = {
+	_name : "a",
+	_age : 1,
+	set name(name){this._name = name},
+	get name(){return this._name},
+	set age(age) {this._age = age},
+	get age() {return this._age},
+}
+
+A.name = "b"
+console.log(A.name)
+
+var context = (function(){
+	var welcomeScene = "a";
+	function Context() {
+		this.welcomeScene = "b";
+	}
+	Context.prototype = {
+		get welcomeScene() {
+			return this.welcomeScene;
+		}
+	}
+	return Context;
+}());
+
+
+console.log(context.welcomeScene);
